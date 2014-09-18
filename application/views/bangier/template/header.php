c850a16edd827c95c665c2a97385761f5ef0a7cc
akus <akus@AkusPC>
2014-09-18 17:56:38 +0200

    Add navbar

--- application/views/bangier/template/header.php
+++ application/views/bangier/template/header.php
@@ -19,7 +19,38 @@
     </head>
     <body>
         <header>
-            <nav>
-                <h1>test css</h1>
+            <nav class="navbar navbar-default" role="navigation">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#"><?php echo $this->config->item('title') ?></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" >
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Glowna</a></li>
+                    <li><a href="#">Najlepsze</a></li>
+                    <li><a href="#">Poczekalnia</a></li>
+                    <li><a href="#">Losuj</a></li>
+                  </ul>
+                  <form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                  </form>
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Log in</a></li>
+                    <li><a href="#">Sign up</a></li>
+                    
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
             </nav>
         </header>
